1c556262245e6b2f7d14cebb14217200c55e07ef
notchxor <notchxor@gmail.com>
2019-07-02 21:20:25 -0300

    modify requests timeout

--- modules/url.py
+++ modules/url.py
@@ -8,12 +8,11 @@ def check_url(target,esperado,log_file):
     logging.basicConfig(filename=log_file,level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
 
     try:
-        r=requests.get(target)
+        r=requests.get(target, timeout=40)
         status=str(r.status_code)
         if status  in  esperado:
             return True
         else:
             return False
     except requests.exceptions.ConnectionError:
-        print("except in url")
         return False
